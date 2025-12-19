eccfec948804ac3a58f9ec812afbd67a71212be1
Stefan Raducan <stefan@192.168.1.2>
2025-12-19 10:31:55 +0000

    instrument random-api with OTel tracing

--- random-api/main.py
+++ random-api/main.py
@@ -2,8 +2,23 @@ import random
 from typing import Dict
 
 from fastapi import FastAPI, HTTPException
+from opentelemetry import trace
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+
+# Setup OpenTelemetry
+resource = Resource.create(attributes={SERVICE_NAME: "random-api"})
+provider = TracerProvider(resource=resource)
+processor = BatchSpanProcessor(OTLPSpanExporter())
+provider.add_span_processor(processor)
+trace.set_tracer_provider(provider)
 
 app: FastAPI = FastAPI()
+
+FastAPIInstrumentor.instrument_app(app)
 
 
 @app.get("/random")  # type: ignore[misc]
